664f0dc7f7e247acaaf35907ad7e83fe37ce522b
Cheruiyotki <wilbornkipkoech@gmail.com>
2025-11-20 00:03:58 +0300

    Your descriptive commit message here

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,11 @@
-import { ChevronDown } from "lucide-react";
+ import { ChevronDown } from "lucide-react";
 import { useEffect, useState } from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { codeExamples, floatingCards } from "../data/CodeExmples";
 
 export default function  Hero() {
     const [mousePosition, setMousePosition] =useState({ x: 0, y: 0 });
+    const [activeTab, setActiveTab] =useState( 'App.jsx' );
 
     useEffect(() => {
         function handleMouseMove(e) {
@@ -39,9 +42,30 @@ export default function  Hero() {
                 </div>
                 <ChevronDown className="w-3 h-3 sm:w-3 sm:h-3 text-gray-300"/>
                 </div>
+                <div className="p-3 sm:p-4 relative h-full">
+                {/* File tabs */}
+                <div className="flex space-x-1 sm:space-x-2 mb-3 sm:mb-4 overflow-x-auto">
+                     <button
+                     onClick={() => setActiveTab("App.jsx")}
+                      className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${ activeTab === "App.jsx" ? "bg-blue-500/30 text-white border-blue-400/20" : " bg-white/5 text-gray-300 border-white/10 hover:bg-white/10" }  transition-all duration-200 whitespace-nowrap`}>App.jsx</button>
+                     <button 
+                     onClick={() => setActiveTab("Hero.jsx")}
+                     className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${ activeTab === "Hero.jsx" ? "bg-blue-500/30 text-white border-blue-400/20" : " bg-white/5 text-gray-300 border-white/10 hover:bg-white/10" }  transition-all duration-200 whitespace-nowrap`}>Hero.jsx</button>
+                     <button
+                     onClick={() => setActiveTab("Navbar.jsx")}
+                      className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border ${ activeTab === "Navbar.jsx" ? "bg-blue-500/30 text-white border-blue-400/20" : " bg-white/5 text-gray-300 border-white/10 hover:bg-white/10" }  transition-all duration-200 whitespace-nowrap`}>Navbar.jsx</button>
+                </div>
+
+                {/* Code Content  */}
+                <div className="relative overflow-hidden flex-grow">
+                  <SyntaxHighlighter language="javascript">
+                    {codeExamples[activeTab] }
+                  </SyntaxHighlighter>
+                </div>
+            </div>
             </div>
         </div>
      </div>
     </section> 
     );
-}
+} 
