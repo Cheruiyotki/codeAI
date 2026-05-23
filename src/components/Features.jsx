4dbebf263aaa4d0c55b7c47b67c086d417270547
Cheruiyotki <wilbornkipkoech@gmail.com>
2026-05-23 03:23:40 +0300

    features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -25,13 +25,26 @@ const features = [
 
 export default function  Features() {
     return  <section id="features" className="py-16 sm:py-20 px-10 sm:px-8 relative">
-        <div>
+        <div className="max-w-6xl mx-auto">
             <div className="text-center mb-12 sm:mb-16 lg:sm-20">
-                <h2>
-                    <span>Your Complete AI-Powered Development Toolkit</span>
+                <h2 className="text-5xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 sm:mb-6">
+                    <span className="bg-gradient-to-b from-white to-gray-300 bg-clip-text text-transparent ">Your Complete AI-Powered Development Toolkit</span>
                     <br />
-                    <span>Workflow Automation</span>
+                    <span className="bg-gradient-to-b from-blue-400 to-cyan-300 bg-clip-text text-transparent ">Workflow Automation</span>
                 </h2>
+            </div>
+
+            <div className="space-y-16 sm:space-y-20 lg:space-y-32">
+                {features.map((feature,  key) => (
+                  <div key={key} className="">
+                    {/*Code section*/}
+                 =   <div>
+                        <div>
+                            
+                        </div>
+                    </div>
+                  </div>
+                ))}
             </div>
         </div>
     </section>;
