3a931594add3762d473179d70d27eb1d5a3ab24f
Cheruiyotki <wilbornkipkoech@gmail.com>
2026-05-26 23:50:08 +0300

    pricing

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -12,12 +12,54 @@ const plans = [
             "Limited API access",
             "Mobile app access"
         ],
-        
+        mostPopular: false
+    },
+    {
+        name: "Pro",
+        price: "$79.99/month",
+        description: "Perfect for growing teams and professionals who need more power and flexibility.",
+        features: [
+            "Up to 20 projects",
+            "100Gb of storage",
+            "Advanced AI model access",
+            "Advanced analytics",
+            "Priority support",
+            "Unlimited API access",
+            "Mobile app access"
+        ],
+        mostPopular: true
+    },
+    {
+        name: "Enterprise",
+        price: " 199.99/month",
+        description: "Tailored solutions for large organizations with custom needs and dedicated support.",
+        features: [
+            "Custom AI model access",
+            "Custom storage options",
+            "Dedicated support",
+            "Unlimited API access",
+            "24/7 support",
+            "Mobile app access"
+        ],
+        mostPopular: false
     }
 ]
 
 
 
 export default function  Pricing() {
-    return <div></div>;
+    return  <section id="pricing" className="py-16 sm:py-20 px-10 sm:px-8 relative">
+        <div className="max-w-7xl mx-auto">
+            <div className="text-center mb-12 sm:mb-16 lg:sm-20">
+                <h2 className="text-5xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 sm:mb-6">
+                    <span className="bg-gradient-to-b from-white to-gray-300 bg-clip-text text-transparent "> Simple, Transparent  </span>
+                    <br />
+                    <span className="bg-gradient-to-b from-blue-400 to-cyan-300 bg-clip-text text-transparent ">Pricing Plans</span>
+                </h2>
+                <p className="text-lg text-gray-400 max-w-2xl sm:text-lg mx-auto">
+                    Choose the perfect plan for your needs. All plans include a 14-day free trial.
+                </p>
+            </div>
+            </div>
+         </section>;
 }
